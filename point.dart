3fef9f5296769034a12298f2c9fafbed127fc408
George <george.hs@hsress.com>
2022-03-11 17:40:01 +0800

    Add factor constructor

--- point.dart
+++ point.dart
@@ -1,22 +1,19 @@
-class Point {
+import 'package:equatable/equatable.dart';
+
+class Point extends Equatable {
   Point(this.x, this.y);
   final int x;
   final int y;
 
-  @override
-  String toString() => 'Point($x, $y)';
-
-  @override
-  bool operator ==(Object other) {
-    if (other is Point) {
-      return x == other.x && y == other.y;
-    }
-    return false;
-  }
-
   Point operator +(covariant Point other) => Point(x + other.x, y + other.y);
 
   Point operator *(covariant int value) => Point(x * value, y * value);
+
+  @override
+  List<Object?> get props => [x, y];
+
+  @override
+  bool? get stringify => true;
 }
 
 void main(List<String> args) {
--- shap.dart
+++ shap.dart
@@ -1,14 +1,35 @@
 import 'dart:math';
 
 abstract class Shape {
+  const Shape();
   double get area;
   double get perimeter;
 
   void printValues() => print('$this, area: $area, perimeter: $perimeter');
+
+  factory Shape.fromJson(Map<String, Object> json) {
+    final type = json['type'];
+    switch (type) {
+      case 'square':
+        final side = json['side'];
+        if (side is double) {
+          return Square(side);
+        }
+        throw UnsupportedError('invalid or missing side property');
+      case 'circle':
+        final radius = json['radius'];
+        if (radius is double) {
+          return Circle(radius);
+        }
+        throw UnsupportedError('invalid or missing radius property');
+      default:
+        throw UnimplementedError('shape $type not recognized');
+    }
+  }
 }
 
 class Square extends Shape {
-  Square(this.side);
+  const Square(this.side);
   final double side;
 
   @override
@@ -19,7 +40,7 @@ class Square extends Shape {
 }
 
 class Circle extends Shape {
-  Circle(this.radius);
+  const Circle(this.radius);
   final double radius;
 
   @override
@@ -29,9 +50,15 @@ class Circle extends Shape {
   double get perimeter => 2 * pi * radius;
 }
 
+void printArea(Shape shape) {
+  print(shape.area);
+}
+
 void main(List<String> args) {
-  final shapes = [Square(4.0), Circle(2.0)];
-  shapes.forEach((element) {
-    element.printValues();
-  });
+  final shapeJson = [
+    {'type': 'square', 'side': 10.0},
+    {'type': 'circle', 'radius': 5.0}
+  ];
+  final shape = shapeJson.map((shapeJson) => Shape.fromJson(shapeJson));
+  shape.forEach(printArea);
 }
