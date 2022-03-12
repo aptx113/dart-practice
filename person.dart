7a5455598d9b57818ef8fe98cfc853b2560cfac0
DanteYu <aptx113@gmail.com>
2022-03-12 15:40:48 +0800

    finish exercise

--- person.dart
+++ person.dart
@@ -2,6 +2,32 @@ class Person {
   Person(this.name, this.age);
   final String name;
   final int age;
+
+  factory Person.fromJson(Map<String, Object> json) {
+    final name = json['name'];
+    final age = json['age'];
+
+    if (name is String && age is int) {
+      if (name.isNotEmpty && age > 0) {
+        return Person(name, age);
+      } else {
+        throw UnsupportedError('invalid value');
+      }
+    } else {
+      throw UnsupportedError('invalid type');
+    }
+  }
+
+  Map<String, Object> toJson() {
+    if (name.isNotEmpty && age > 0) {
+      return {'name': name, 'age': age};
+    }
+    throw UnimplementedError('Person $name not recognize');
+  }
 }
 
-void main() {}
+void main() {
+  final person = Person.fromJson({'name': 'Andrea', 'age': 36});
+  final json = person.toJson();
+  print(json);
+}
