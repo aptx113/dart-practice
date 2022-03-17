0bd3affb869bac213246f6c484cbd03fa2dcb22a
George <george.hs@hsress.com>
2022-03-17 16:06:23 +0800

    Finish error and exception part

---
+++ errors-exceptions/exceptions.dart
@@ -0,0 +1,30 @@
+class Fraction {
+  Fraction(this.numerator, this.denominator) {
+    if (denominator == 0) {
+      throw UnsupportedError('IntegerDivisionByZeroException');
+    }
+  }
+  final int numerator;
+  final int denominator;
+
+  double get value => numerator / denominator;
+}
+
+void testFraction() {
+  try {
+    final f = Fraction(3, 0);
+    print(f.value);
+  } on UnsupportedError catch (e) {
+    print(e);
+    rethrow;
+  } on Exception catch (e) {
+    print(e);
+  } finally {
+    print('testFraction done');
+  }
+}
+
+void main() {
+  testFraction();
+  print('Done');
+}
